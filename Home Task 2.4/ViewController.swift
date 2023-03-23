9615dd210ac238c38a87fdfafe65ed4e47b8b989
polosa00 <i@apolochanin.ru>
2023-03-23 23:28:48 +0300

    Initial Commit

---
+++ Home Task 2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home Task 2.4
+//
+//  Created by Александр Полочанин on 23.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

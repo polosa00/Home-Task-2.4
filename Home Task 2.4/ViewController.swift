2a13d9d4f5205efe7f1fa5760d8b7604f2df572c
polosa00 <i@apolochanin.ru>
2023-03-25 00:32:41 +0300

    Finished Text Field logic

--- Home Task 2.4/ViewController.swift
+++ Home Task 2.4/ViewController.swift
@@ -15,11 +15,14 @@ class ViewController: UIViewController {
     @IBOutlet var greenChanelLabel: UILabel!
     @IBOutlet var blueChanelLabel: UILabel!
     
-    
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
-
+    
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -35,6 +38,87 @@ class ViewController: UIViewController {
         setupLabelValues()
         setupColorValues()
     }
+    
+    @IBAction func tappedRedButton() {
+        guard let inputText = redTextField.text, !inputText.isEmpty else {
+            showAlert(withTitle: "Поле для ввода пустое!", andMessage: "Пожалуйста, введите данные")
+            return
+        }
+        
+        if let inputValue = redTextField.text, let floatValue = Float(inputValue) {
+            redSlider.value = floatValue
+            redChanelLabel.text = String(floatValue)
+            if floatValue > 255 {
+                showAlert(withTitle: "Вы ввели значение превышающее 255!", andMessage: "Введите значение от 0 до 255")
+            }
+        }
+        
+        let chanelValuePattern = "^[0-9]{0,3}$"
+        let isChanelValueValid = NSPredicate(format: "SELF MATCHES %@", chanelValuePattern)
+            .evaluate(with: inputText)
+        
+        if !isChanelValueValid {
+            showAlert(
+                withTitle: "Неверный формат",
+                andMessage: "Введите значение от 0 до 255"
+            )
+            return
+        }
+        
+        setupColorValues()
+    }
+    
+    @IBAction func tappedGreenButton() {
+        guard let inputText = greenTextField.text, !inputText.isEmpty else {
+            showAlert(withTitle: "Поле для ввода пустое!", andMessage: "Пожалуйста, введите данные")
+            return
+        }
+        
+        if let inputValue = greenTextField.text, let floatValue = Float(inputValue) {
+            greenSlider.value = floatValue
+            greenChanelLabel.text = String(floatValue)
+            if floatValue > 255 {
+                showAlert(withTitle: "Вы ввели значение превышающее 255!", andMessage: "Введите значение от 0 до 255")
+            }
+        }
+        
+        let chanelValuePattern = "^[0-9]{0,3}$"
+        let isChanelValuePattern = NSPredicate(format: "SELF MATCHES %@", chanelValuePattern)
+            .evaluate(with: inputText)
+        
+        if !isChanelValuePattern {
+            showAlert(withTitle: "Неверный формат", andMessage: "Введите значение от 0 до 255")
+        }
+        
+        setupColorValues()
+    }
+    
+    @IBAction func tappedBlueButton() {
+        guard let inputText = blueTextField.text, !inputText.isEmpty else {
+            showAlert(withTitle: "Поле для ввода пустое!", andMessage: "Пожалуйста, введите данные")
+            return
+        }
+        
+        if let inputValue = blueTextField.text, let floatValue = Float(inputValue) {
+            blueSlider.value = floatValue
+            blueChanelLabel.text = String(floatValue)
+            if floatValue > 255 {
+                showAlert(withTitle: "Вы ввели значение превышающее 255!", andMessage: "Введите значение от 0 до 255")
+            }
+        }
+        
+        let chanelValuePattern = "^[0-9]{0,3}$"
+        let isChanelValuePattern = NSPredicate(format: "SELF MATCHES %@", chanelValuePattern)
+            .evaluate(with: inputText)
+        
+        if !isChanelValuePattern {
+            showAlert(withTitle: "Неверный формат", andMessage: "Введите значение от 0 до 255")
+        }
+        
+        setupColorValues()
+        
+    }
+    
 }
 
 extension ViewController {
@@ -51,6 +135,28 @@ extension ViewController {
             blue: CGFloat(blueSlider.value) / 255,
             alpha: 1
         )
+    }
+}
+
+extension ViewController {
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.redTextField.text = ""
+            self.redChanelLabel.text = "0.00"
+            self.redSlider.value = 0
+            
+            self.greenTextField.text = ""
+            self.greenChanelLabel.text = "0.00"
+            self.greenSlider.value = 0
+            
+            self.blueTextField.text = ""
+            self.blueChanelLabel.text = "0.00"
+            self.blueSlider.value = 0
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+
     }
 }
 
